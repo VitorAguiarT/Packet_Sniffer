0b4c7c1846dfbf236baad6edabb19dd635b9570f
VitorAguiarT <vat2@cin.ufpe.br>
2021-02-11 23:59:28 -0300

    first commit

---
+++ packet_sniffer.py
@@ -0,0 +1,38 @@
+import pyshark
+import wget
+
+def capturePackets(networkInterface):
+    """  
+    Captura pacotes continuamente da interface e printa os pacotes HTTPs filtrados.
+    """
+    capturePacket = pyshark.LiveCapture(interface=networkInterface, display_filter='http')
+    for preFilterPacket in capturePacket.sniff_continuously():
+        filteredPacket = filterHTTP(preFilterPacket)
+        try:
+            url = filteredPacket['http']._all_fields['http.request.full_uri']
+            if '.jpg' or '.jpeg' or '.png' in url:
+                if('Python-urllib/3.8' in filteredPacket['http']._all_fields['http.user_agent']):
+                    pass
+                else:
+                    print(filteredPacket['http']._all_fields['http.user_agent'])
+                    saveImage(url)
+            else:
+                pass
+        except:
+            pass
+
+def filterHTTP(packet):
+    """
+    Filtra pacotes HTTPs.
+    """
+    if hasattr(packet, 'http'):    
+        return packet
+
+def saveImage(url):
+    """
+    Baixa a imagem diretamente da url capturada.
+    """
+    fileName = wget.download(url, out="./Captured_Images")
+    print('\nDownloaded image name:', fileName)
+
+capturePackets('wlp3s0')
